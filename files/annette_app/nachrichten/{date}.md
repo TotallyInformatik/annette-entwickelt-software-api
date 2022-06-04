90e59e26134b72c623a50d204ac3efbb9fdf6f24
Rui Zhang <totallyturing@gmail.com>
2022-06-04 15:50:14 +0200

    Dateien und Nachrichten System konfiguriert

---
+++ files/annette_app/nachrichten/{date}.md
@@ -0,0 +1,5 @@
+# Überschrift
+
+Lorem Ipsum Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text
+
+![image alt text](https://www.bsv-duesseldorf.de/static/media/167667838_234765408429645_5699080646736834876_n-850x850.545d9d1c24d7e4c0c099.jpg)
--- pages/api/annette_app/dateien/klausur_plan/[klasse].ts
+++ pages/api/annette_app/dateien/klausur_plan/[klasse].ts
@@ -7,20 +7,32 @@ import path from "path";
 
 import { NextApiRequest, NextApiResponse } from "next";
 
+/**
+ * @param req ..
+ * @param res ..
+ * 
+ * falls die "query" ef, q1 oder q2 ist, dann wird der entsprechende Klausurplan geschickt
+ * 
+ */
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
 
+  const akzeptierteKlassen = ["ef", "q1", "q2"];
   const { klasse } = req.query;
 
-  console.log("datei: " + klasse);
+  if (!akzeptierteKlassen.includes(klasse.toString())) {
+    res.status(404).json({message: "no such file"});
+    return;
+  }
 
   const filePath = path.resolve(".", `files/annette_app/klausur_${klasse}.pdf`);
   const imageBuffer = fs.readFileSync(filePath);
 
   res.setHeader("Content-Type", "application/pdf"); 
   //* correct "MIME type" is application/pdf
-  res.send(imageBuffer);
+  res.status(200).send(imageBuffer);
+  return;
 
 }
