24902a40050f3ec1434304fd57af9375d1dc2b2c
123654-dev <72887184+123654-dev@users.noreply.github.com>
2023-02-12 15:49:00 +0100

    Caching für Klassen-Optionen

--- pages/api/annette_app/dateien/klassen_optionen.ts
+++ pages/api/annette_app/dateien/klassen_optionen.ts
@@ -1,22 +1,38 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import cache from "memory-cache";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const WebUntisLib = require("webuntis");
+  const cachedResult = cache.get("klassen");
+  if (cachedResult) {
+    console.log("Serving cached result for", req.url);
+    res.status(200).json(cachedResult);
+  } else {
+    const WebUntisLib = require("webuntis");
 
-  const untis = new WebUntisLib.WebUntisAnonymousAuth(
-    "avdhg-duesseldorf",
-    "ajax.webuntis.com"
-  );
+    const untis = new WebUntisLib.WebUntisAnonymousAuth(
+      "avdhg-duesseldorf",
+      "ajax.webuntis.com"
+    );
 
-  await untis.login();
-  const classes = await untis.getClasses();
-  const classesFmt = [];
-  for(const element of classes) {
-    const klasse = element;
-    classesFmt.push(klasse["name"]);
+    await untis.login();
+    const classes = await untis.getClasses();
+    const classesFmt = [];
+    for (const element of classes) {
+      const klasse = element;
+      classesFmt.push(klasse["name"]);
+    }
+
+    const later = new Date();
+    //one week cache
+    later.setDate(new Date().getDate() + 7);
+    cache.put(
+      "klassen",
+      classesFmt,
+      later.getTime() - new Date().getTime()
+    );
+    res.status(200).json(classesFmt);
   }
-  res.status(200).json(classesFmt);
 }
